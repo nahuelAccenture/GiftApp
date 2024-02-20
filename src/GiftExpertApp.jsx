e3e4802685a29c4d078c2fb68cf1ce6005d68a79
NahuelAlvarez <nalvarez23@live.com.ar>
2024-02-20 20:22:37 -0300

    First Commit

---
+++ src/GiftExpertApp.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { AddCategory, GiftGrid } from "./components";
+
+
+export const GiftExpertApp = () => {
+
+const [categories, setCategories] = useState(['One Punch']);
+
+const onAddCategory = (newCategory) => {
+
+  if(categories.includes(newCategory)) return;
+
+  setCategories([newCategory, ...categories]);
+
+}
+  return (
+    <>
+      <h1>GiftExpertApp</h1>
+      <AddCategory 
+        onNewCategory = {(value) => onAddCategory(value) }
+      />
+    
+      {
+        categories.map( category => (
+          <GiftGrid 
+            key={category} 
+            category={category}
+          />
+          ))
+      }
+   
+    </>
+  );
+};
---
+++ src/helpers/getGifts.js
@@ -0,0 +1,15 @@
+export const getGifts = async (category) => {
+        
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=3cPv3yn2mZck8LQIp2aDLdednswiIuVF&q=${category}&limit=10`;
+
+    const resp = await fetch( url );
+    const { data } = await resp.json();
+
+    const gifs = data.map( img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }));
+    
+    return gifs;
+}
